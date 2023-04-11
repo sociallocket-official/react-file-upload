45c2ff15131e025b9bffde1ab114ae8c25939f9a
sociallocket-offical <sociallocket.projects@gmail.com>
2023-04-11 23:41:23 +0100

    added upload front end test

---
+++ react-file-upload/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function App() {
+
+  const [file, setFile] = useState()
+
+  function handleChange(event) {
+    setFile(event.target.files[0])
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    //REACT_APP_URL="http://ec2-52-56-97-212.eu-west-2.compute.amazonaws.com/api/v1/"
+
+    const url = 'http://localhost:3002/api/v1/files';
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('fileName', file.name);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+    axios.post(url, formData, config).then((response) => {
+      console.log(response.data);
+    });
+
+  }
+
+  return (
+    <div className="App">
+        <form onSubmit={handleSubmit}>
+          <h1>React File Upload</h1>
+          <input type="file" onChange={handleChange}/>
+          <button type="submit">Upload</button>
+        </form>
+    </div>
+  );
+}
+
+export default App;
