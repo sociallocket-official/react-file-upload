46bf7085c95c749fcb125622ec1759046d37f469
sociallocket-offical <sociallocket.projects@gmail.com>
2023-05-24 21:23:28 +0400

    test upload in live server env

--- react-file-upload/src/App.js
+++ react-file-upload/src/App.js
@@ -15,7 +15,8 @@ function App() {
     //REACT_APP_URL="http://ec2-52-56-97-212.eu-west-2.compute.amazonaws.com/api/v1/"
 
     //const url = 'http://localhost:3002/api/v1/files';
-    const url = 'https://test.app.sociallocket.com/api/v1/files'; //process.env.REACT_APP_URL + "files";
+    //const url = process.env.REACT_APP_URL + "files";
+    const url = 'https://test.app.sociallocket.com/api/v1/files';
     const formData = new FormData();
     formData.append('file', file);
     formData.append('fileName', file.name);
