0abb2f9b599cac72389b6f52d93adadce97590df
sociallocket-offical <sociallocket.projects@gmail.com>
2023-07-17 18:48:42 +0400

    updated url for app to post data to (green env)

--- react-file-upload/src/App.js
+++ react-file-upload/src/App.js
@@ -14,9 +14,9 @@ function App() {
     event.preventDefault()
     //REACT_APP_URL="http://ec2-52-56-97-212.eu-west-2.compute.amazonaws.com/api/v1/"
 
-    //const url = 'http://localhost:3002/api/v1/files';
+    const url = 'https://green.app.sociallocket.com/api/v1/files';
     //const url = process.env.REACT_APP_URL + "files";
-    const url = 'https://test.app.sociallocket.com/api/v1/files';
+    //const url = 'https://test.app.sociallocket.com/api/v1/files';
     const formData = new FormData();
     formData.append('file', file);
     formData.append('fileName', file.name);
